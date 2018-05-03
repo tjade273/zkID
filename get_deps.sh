c28d8cf08006d2101caf6fc9ac6f4e304e03023d
Tjaden Hess <tah83@cornell.edu>
2018-05-02 23:00:29 -0400

    Tweak deps for travis

--- get_deps.sh
+++ get_deps.sh
@@ -1,4 +1,16 @@
-sudo apt-get install build-essential cmake git libgmp3-dev libprocps3-dev python-markdown libboost-all-dev libssl-dev libgtest-dev libjsonrpccpp-dev libjsonrpccpp-tools
+sudo apt-get install build-essential cmake git libgmp3-dev libprocps3-dev python-markdown libboost-all-dev libssl-dev libgtest-dev
+
+
+sudo apt-cache show libjsonrpccpp-dev
+
+if [$? -ne 0];
+then
+    sudo apt-get install libjsonrpccpp-dev libjsonrpccpp-tools
+else
+    sudo apt-get install libjson-rpc-cpp-dev libjson-rpc-cpp-tools
+fi
+
+
 git submodule init && git submodule update
 cd /usr/src/gtest/
 sudo cmake CMakeLists.txt
