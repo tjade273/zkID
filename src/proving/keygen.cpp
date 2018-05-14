3123a0693165d51f81b720cee44da7ebe625e6f4
Tjaden Hess <tah83@cornell.edu>
2018-05-14 01:37:18 -0400

    Implement key generation and verifier export

--- src/proving/keygen.cpp
+++ src/proving/keygen.cpp
@@ -9,4 +9,7 @@ int main(int argc, char* argv[])
   libff::alt_bn128_pp::init_public_params();
   zkidProverImpl<sha256_two_to_one_hash_gadget> zkid(tree_depth, 32);
   zkid.ExportKeys(argv[2]);
+  auto vk_pp = zkid.GetVerificationKey();
+  VerificationKey vk = ExtractVerificationKey(vk_pp);
+  ExportVerificationKey(vk, "test/res/verification.sol.tmpl", std::string(argv[2])+"/verification.sol");
 }
